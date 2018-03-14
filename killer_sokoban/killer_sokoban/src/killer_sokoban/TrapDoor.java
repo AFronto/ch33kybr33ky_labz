b034f2562126054f90b7125746cf3a1ffc8d048b
Motori Spániel <denyjol@MOTORI-ACER.localdomain>
2018-03-14 13:25:19 +0100

    Button es TrapDoor aktivalo fuggvenyek.

--- killer_sokoban/killer_sokoban/src/killer_sokoban/TrapDoor.java
+++ killer_sokoban/killer_sokoban/src/killer_sokoban/TrapDoor.java
@@ -10,9 +10,15 @@ public class TrapDoor extends Hole{
 		printOnConstruct("TrapDoor");
 		printOnExitConstuctor("TrapDoor");
 	}
-
+	
+	/**
+	 * Beallitja hogy aktiv-e a trapdoor.
+	 * @param b isActive valtozo erteke erre allitodik.
+	 */
 	public void SetActive(boolean b){
+		printOnEntry(this, "SetActive", b+"");
 		isActive = b;
+		printOnExit(this, "SetActive", null);
 	}
 	
 	public void FieldAction(){
